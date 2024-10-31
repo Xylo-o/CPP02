c9713e68389a78c1594335c6250692fc8d590707
Xyloo <adprzyby@student.42heilbronn.de>
2024-10-31 17:37:32 +0100

    Added files and some skeletons

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/31 17:01:31 by adprzyby          #+#    #+#             */
+/*   Updated: 2024/10/31 17:32:36 by adprzyby         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <string>
+#include <iostream>
+
+class Fixed {
+	public:
+		
+	private:
+		int value = 0;
+		static const int bits = 8; 
+};
