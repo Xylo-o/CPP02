829e8414d41ca6ea4a20018bca300fdf66367d73
Xyloo <adprzyby@student.42heilbronn.de>
2024-11-02 18:40:48 +0100

    Implemented main from the subject + added text notification when calling getRawBits + changed the name of the program

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:01:17 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/11/02 16:47:38 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/11/02 18:34:11 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 const int Fixed::bits = 8;
 
-int getRawBits( void ) const {
-	
+int Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits function called\n";
+	return this->value;
 }
 
-void setRawBits( int const raw ) {
-	
+void Fixed::setRawBits( int const raw ) {
+	this->value = raw;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,18 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 17:01:35 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/11/02 16:38:04 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/11/02 17:55:34 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
-int main (void) {
-	Fixed obj1;
-	Fixed obj2;
-	obj2 = obj1;
-	Fixed obj3 = obj1;
-	return (0);
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
 }
