4525805740c42c9c5872d4ca7fdbc7fe5dcc7538
Xyloo <adprzyby@student.42heilbronn.de>
2024-11-03 15:39:29 +0100

    Created overloaded << operator + created convertion functions

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,18 +6,32 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 18:43:28 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/11/02 18:50:03 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/11/03 15:37:24 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+const int Fixed::bits = 8;
+
+int Fixed::getRawBits( void ) const {
+	std::cout << "getRawBits function called\n";
+	return value;
+}
+
+void Fixed::setRawBits( int const raw ) {
+	this->value = raw;
+}
+
 float Fixed::toFloat( void ) const {
-	int value = 0;
-	return (value);
+	return (static_cast<float>(value) / (1 << bits));
 }
 
 int Fixed::toInt( void ) const {
-	int value = 0;
-	return (value);
+	return (static_cast<int>(value) / (1 << bits));
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+    os << fixed.toFloat();
+    return os;
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: adprzyby <adprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 18:43:25 by adprzyby          #+#    #+#             */
-/*   Updated: 2024/11/02 18:55:42 by adprzyby         ###   ########.fr       */
+/*   Updated: 2024/11/03 15:35:00 by adprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,35 @@
 
 class Fixed {
 	public:
-		float toFloat( void ) const;
-		int toInt( void ) const;
-		Fixed( const int i) {
-			std::cout << "Int consturctor called\n";
+		Fixed() : value(0) {
+			std::cout << "Default constructor called\n"; 
 		}
-		Fixed( const float f) {
+		Fixed( const int i) : value(i << bits) {
+			std::cout << "Int constructor called\n";
+		}
+		Fixed( const float f) : value(static_cast<int>(roundf(f * (1 << bits)))) {
 			std::cout << "Float constructor called\n";
 		}
-		Fixed() {
-			std::cout << "Default constructor called\n";
+		Fixed(const Fixed&other) : value(other.value) {
+			std::cout << "Copy constructor called\n";
+		}
+		Fixed& operator=(const Fixed& other) {
+			std::cout << "Copy assignment operator called\n";
+			if (this == &other) {
+				return *this;
+			}
+			value = other.value;
+			return (*this);
 		}
 		~Fixed() {
 			std::cout << "Destructor has been called\n";
 		}
+		friend std::ostream& operator<<(std::ostream& os, const Fixed& fixed);
+		int getRawBits( void ) const;
+		void setRawBits( int const raw );
+		float toFloat( void ) const;
+		int toInt( void ) const;
 	private:
+		int value;
+		static const int bits; 
 };
